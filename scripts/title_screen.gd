ae484e123cb0f67441b59e05f7807504e58fe62c
eskally <ela.skalic@student.um.si>
2025-05-04 19:50:50 +0200

    Changed play button action (opens idle screen)

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://screens/battle.tscn")
+	get_tree().change_scene_to_file("res://screens/idle_screen.tscn")
 
 func _on_exit_pressed():
 	get_tree().quit()
