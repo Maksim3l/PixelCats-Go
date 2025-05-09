c403dcb96c490a39897aa4df6339f02c0f784510
KatjaGHub <katja.slekovec@student.um.si>
2025-05-10 00:22:08 +0200

    Changed the tornado sprite to the new cloud one, fixed a bit of the positioning when merging cats

--- scripts/merge.gd
+++ scripts/merge.gd
@@ -4,6 +4,8 @@ const TORNADO = preload("res://screens/tornado.tscn")
 @onready var cat2: CharacterBody2D = $CharacterBody2D2
 @onready var tornado: CharacterBody2D = $tornado
 @onready var timer: Timer = $Timer
+@onready var unlock = $unlock
+@onready var tornado_animated_sprite: AnimatedSprite2D
 
 var selected_cats = []
 var cat1_data
@@ -21,6 +23,17 @@ func _ready():
 	boost_stats()
 	
 	tornado.hide()
+	if tornado.has_node("AnimatedSprite2D"):
+		var found_sprite = tornado.get_node("AnimatedSprite2D")
+		if found_sprite is AnimatedSprite2D:
+			tornado_animated_sprite = found_sprite
+		else:
+			printerr("Node 'AnimatedSprite2D' inside tornado is not of type AnimatedSprite2D!")
+	else:
+		printerr("Tornado node does not have a child named 'AnimatedSprite2D'!")
+	if not tornado_animated_sprite:
+		printerr("Failed to get tornado_animated_sprite in _ready(). Animation might not play.")
+	
 	cat1.connect("collision_detected", Callable(self, "_on_cat_collision"))
 	if not timer.timeout.is_connected(_on_tornado_timeout):
 		timer.timeout.connect(_on_tornado_timeout)
@@ -54,15 +67,22 @@ func _on_cat_collision():
 	cat2.queue_free()
 	
 	tornado.show()
+	if tornado_animated_sprite: 
+		tornado_animated_sprite.frame = 0 
+		tornado_animated_sprite.play("default") 
+	else:
+		printerr("Cannot play tornado animation: tornado_animated_sprite is not set!")
 	
-	timer.wait_time = 4.0
+	timer.wait_time = 0.6
 	timer.one_shot = true
 	timer.start()
 	
 func _on_tornado_timeout():
 	print("Tornado will be hidden")
+	unlock.play()
 	tornado.hide()
 	cat1.visible = true
+	
 	await get_tree().create_timer(1.2).timeout
 	
 	if GlobalDataHandler.global_data.coming_from_last == "Battle Arena":
